c23461b534b7dbce2498c30a7ed7942ea19c9acf
WRFitch <WRFitch@outlook.com>
2019-02-04 20:54:03 +0000

    Updated FindRestaurantActivity & SplashPageActivity & added app icon

--- app/src/main/java/com/example/android/tabl/FindRestaurantActivity.java
+++ app/src/main/java/com/example/android/tabl/FindRestaurantActivity.java
@@ -1,8 +1,17 @@
 package com.example.android.tabl;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.pm.PackageManager;
+import android.Manifest;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,12 +33,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * TODO: implement additional search method.
  */
 
-/**
- * TODO: implement button function
- */
-
 public class FindRestaurantActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private FusedLocationProviderClient mFusedLocationClient;
     private GoogleMap mMap;
 
     @Override
@@ -40,14 +46,32 @@ public class FindRestaurantActivity extends FragmentActivity implements OnMapRea
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        /*
+        if(ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION ) !=
+                PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( this, new String[] {
+                            android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    );
+        }*/
+
+        FloatingActionButton fab = findViewById(R.id.snapToLocationButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snapToCurrentLocation(view);
+            }
+        });
+
+    }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -56,13 +80,80 @@ public class FindRestaurantActivity extends FragmentActivity implements OnMapRea
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng userLocation = new LatLng(-34, 151);
+
+        //get nearby restaurants and mark on map
+        mMap.addMarker(new MarkerOptions().position(userLocation).title("Marker in Sydney"));
+
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
     }
 
     public void snapToCurrentLocation(){
+        //get user location from phone
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(getUserLocation()));
+    }
+
+    public LatLng getUserLocation(){
+        return null;
+    }
+
+    //This method is bad and I should be ashamed.
+    //consider implementing point focus if required
+    public void snapToCurrentLocation(View view){
+        //CameraUpdate currentLocation = mMap.get;
+
+        /*
+        Location l = mFusedLocationClient.getLastLocation();
+        LatLng currentLocation = new LatLng(mFusedLocationClient.getLastLocation());
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 0));
+        */
+
+        //this code will be useful in future for basically anything
+        Snackbar.make(view, getString(R.string.todo),
+                Snackbar.LENGTH_LONG).setAction(R.string.todo, null)
+                .show();
+    }
+
+    public LatLng[] getRestaurantsInRadius(){
+        return null;
+    }
+
+    /**
+     * /////////////////////////////////////////////////////////////////////////////////////////////
+     *
+     * OLD CODE FROM STARTPAGE
+     *
+     * /////////////////////////////////////////////////////////////////////////////////////////////
+     */
+
+    /*
+
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_page);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        /*
+        if(ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION ) !=
+                PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( this, new String[] {
+                    android.Manifest.permission.ACCESS_FINE_LOCATION  },
+                    );
+        }
+        *//*
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
+    */
 }
--- app/src/main/java/com/example/android/tabl/SplashPageActivity.java
+++ app/src/main/java/com/example/android/tabl/SplashPageActivity.java
@@ -1,83 +1,28 @@
 package com.example.android.tabl;
 
 /**
- * TODO: This is now the splashscreen, not the StartPage.
- * TODO: Refactor to fit SplashScreen - change to image fragment?
- * TODO: copy useful content into FindRestaurantActivity
+ * Splash Page for TABL
+ *
+ * A simple image that users swipe away after a second to start application. Alters usability and
+ * hides small optimisation getting local restaurant data in preparation for FindRestaurantActivity.
+ *
+ * @WRFitch
  */
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+/**
+ * TODO: This is now the splashscreen, not the StartPage.
+ * TODO: Refactor to fit SplashScreen - change to image fragment?
+ */
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class SplashPageActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
-    private FusedLocationProviderClient mFusedLocationClient;
+public class SplashPageActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_page);
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-        /*
-        if(ContextCompat.checkSelfPermission(
-                this, android.Manifest.permission.ACCESS_FINE_LOCATION ) !=
-                PackageManager.PERMISSION_GRANTED ) {
-            ActivityCompat.requestPermissions( this, new String[] {
-                    android.Manifest.permission.ACCESS_FINE_LOCATION  },
-                    );
-        }
-        */
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                snapToCurrentLocation(view);
-            }
-        });
+        setContentView(R.layout.activity_select_table);
     }
-
-    //consider implementing point focus if required
-    public void snapToCurrentLocation(View view){
-        //CameraUpdate currentLocation = mMap.get;
-        /*
-        Location l = mFusedLocationClient.getLastLocation();
-        LatLng currentLocation = new LatLng(mFusedLocationClient.getLastLocation());
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 0));
-        */
-        Snackbar.make(view, getString(R.string.function_todo),
-                Snackbar.LENGTH_LONG).setAction("whoops", null)
-                .show();
-    }
-
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
 
 }
