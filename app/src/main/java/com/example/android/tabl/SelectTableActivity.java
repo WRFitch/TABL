d7e24257d8f8b5b2122d4785f44ead566c750958
ednk10 <1708519@brunel.ac.uk>
2019-02-10 20:17:05 +0000

    past orders list has been simplified

--- app/src/main/java/com/example/android/tabl/SelectTableActivity.java
+++ app/src/main/java/com/example/android/tabl/SelectTableActivity.java
@@ -9,5 +9,10 @@ public class SelectTableActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_table);
+
+        // top action bar text is renamed and a 'close' symbol is enabled
+        this.getSupportActionBar().setTitle("Select Table");
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_close);
     }
 }
