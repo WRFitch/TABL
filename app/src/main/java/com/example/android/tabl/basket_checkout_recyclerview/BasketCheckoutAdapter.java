52791881e22a16d979f68850ae1b6889920687ca
monikapatel1 <1606399@brunel.ac.uk>
2019-02-11 14:46:44 +0000

    Completed basketCheckout class for recycler and now trying to do the Adapter for it (incomplete)

--- app/src/main/java/com/example/android/tabl/basket_checkout_recyclerview/BasketCheckoutAdapter.java
+++ app/src/main/java/com/example/android/tabl/basket_checkout_recyclerview/BasketCheckoutAdapter.java
@@ -1,4 +1,15 @@
 package com.example.android.tabl.basket_checkout_recyclerview;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.tabl.R;
+import com.example.android.tabl.restaurant_recyclerview.Restaurant;
+
+import java.util.List;
+
 public class BasketCheckoutAdapter {
-}
+       }
