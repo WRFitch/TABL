a8ded5ef4d9accab68001b5dbfb03ca0e06a68cd
monikapatel1 <1606399@brunel.ac.uk>
2019-02-12 23:56:13 +0000

    Added Quantity to BasketCheckoutAdapter and in checkout_list_item.xml

--- app/src/main/java/com/example/android/tabl/CheckoutActivity.java
+++ app/src/main/java/com/example/android/tabl/CheckoutActivity.java
@@ -2,12 +2,45 @@ package com.example.android.tabl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class CheckoutActivity extends AppCompatActivity {
 
+    private Button cancelOrderButton, HelpWithOrderButton, addTipButton;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
-    }
+
+        // Cancel Order button will open BasketActivity
+        cancelOrderButton = findViewById(R.id.CancelOrderButton);
+        cancelOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BasketActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+// NEED TO MAKE HELP ACTIVITY AND CLASS SO I CAN LINK THIS BUTTON TO IT, LINKED IT TO 'MORE' PAGE FOR NOW
+        HelpWithOrderButton = findViewById(R.id.HelpWithOrderButton);
+        HelpWithOrderButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), MoreActivity.class); //HERE^
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+    });
+
+
+
 }
+}
+
+        
